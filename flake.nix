09357bb9179dee879f81c9a6efc81c457bf51d97
haruki7049 <tontonkirikiri@gmail.com>
2026-03-24 16:58:26 +0900

    Create flakes

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  inputs = {
+    # nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    # Nix flakes' utilities
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    # treefmt-nix: Treefmt for Nix flakes
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        let
+          buildInputs = [ ];
+          nativeBuildInputs = [
+            pkgs.nil # Nix LSP
+            pkgs.clang-tools # C / C++ toolchain
+            pkgs.gnumake # GNU Make
+          ];
+        in
+        {
+          treefmt = {
+            projectRootFile = ".git/config";
+
+            # Nix
+            programs.nixfmt.enable = true;
+
+            # GitHub Actions
+            programs.actionlint.enable = true;
+
+            # Markdown
+            programs.mdformat.enable = true;
+
+            # ShellScript
+            programs.shellcheck.enable = true;
+            programs.shfmt.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            inherit buildInputs nativeBuildInputs;
+
+            LIBCLANG_PATH = lib.makeLibraryPath buildInputs;
+            LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+
+            inputsFrom = [ config.treefmt.build.devShell ];
+
+            shellHook = ''
+              export PS1="\n[nix-shell:\w]$ "
+            '';
+          };
+        };
+    };
+}
