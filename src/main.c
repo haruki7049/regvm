22b639136b8e8da4fb5a548d333f59cb4c6d05c2
haruki7049 <tontonkirikiri@gmail.com>
2026-03-24 22:06:22 +0900

    Update

--- src/main.c
+++ src/main.c
@@ -1,18 +1,27 @@
+#include "./main.h"
 #include "./vm.h"
 #include <argtable3.h>
 #include <stdio.h>
 
-const int exitcode = 0;
+struct arg_lit *help, *version;
+struct arg_end *end;
+
 const char program_name[] = "regvm";
 
-typedef struct {
-  arg_lit_t *help;
-} Cli;
+int main(int argc, char *argv[]) {
+  void *argtable[] = {
+      help = arg_lit0("h", "help", "display this help and exit"),
+      version = arg_lit0("V", "version", "display version info and exit"),
 
-int main() {
-  Cli cli = {
-      arg_litn("h", "help", 0, 1, "display this help and exit"),
+      end = arg_end(20),
   };
+  int nerrors = arg_parse(argc, argv, argtable);
+
+  if (help->count > 0) {
+    return display_help_message(argtable);
+  } else if (nerrors > 0) {
+    return display_error_message(argtable, program_name);
+  }
 
   RegVM vm = create_reg_vm();
   printf("&vm: %p\n", &vm);
@@ -21,7 +30,27 @@ int main() {
   printf("vm.r2: %d\n", vm.r2);
   printf("vm.r3: %d\n", vm.r3);
   printf("\n");
-
   printf("Hello, it's regvm!!\n");
+
+  arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
   return 0;
+}
+
+int display_help_message(void *argtable) {
+  printf("Usage: %s", program_name);
+  arg_print_syntax(stdout, argtable, "\n");
+  printf("Demonstrate command-line parsing in argtable3.\n\n");
+  arg_print_glossary(stdout, argtable, "  %-25s %s\n");
+
+  arg_freetable(argtable, sizeof(argtable));
+  return 0;
+}
+
+int display_error_message(void *argtable, const char program_name[]) {
+  /* Display the error details contained in the arg_end struct.*/
+  arg_print_errors(stdout, end, program_name);
+  printf("Try '%s --help' for more information.\n", program_name);
+
+  arg_freetable(argtable, sizeof(argtable));
+  return 1;
 }
---
+++ src/main.h
@@ -0,0 +1,3 @@
+int display_help_message(void *argtable);
+
+int display_error_message(void *argtable, const char program_name[]);
