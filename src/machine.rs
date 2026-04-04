46458c33468268ee0b98ccb861cd3f1df7874db5
haruki7049 <tontonkirikiri@gmail.com>
2026-04-05 06:22:43 +0900

    Update

--- src/machine.rs
+++ src/machine.rs
@@ -1,51 +1,129 @@
+use thiserror::Error;
+
 #[derive(Debug, Default, PartialEq, Eq)]
 pub struct RegVM {
-    pc: usize,
-    r0: usize,
-    r1: usize,
-    r2: usize,
-    r3: usize,
+    pc: ProgramCounter,
+    registers: Vec<Register>,
+    instructions: Vec<Instruction>,
+}
+
+impl RegVM {
+    pub fn new(registers: Vec<Register>, instructions: Vec<Instruction>) -> Self {
+        RegVM {
+            registers,
+            instructions,
+            ..Default::default()
+        }
+    }
 }
 
 pub trait CounterMachine {
-    fn run(&mut self, instructions: Vec<Instruction>);
+    fn run(&mut self) -> Result<(), Box<dyn std::error::Error>>;
 }
 
 impl CounterMachine for RegVM {
-    fn run(&mut self, instructions: Vec<Instruction>) {
+    fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         loop {
-            let mut instr = &instructions[self.pc];
-            let mut op = instr.r;
+            let instr = &self.instructions[self.pc];
+
+            match instr {
+                Instruction::Dec { r, next, branch } => {
+                    if self.registers[*r] > 0 {
+                        // Decrements the r register
+                        self.registers[*r] -= 1;
+                        self.pc = *next; // Go to next address
+                    } else {
+                        self.pc = *branch; // Go to branch address
+                    }
+                }
+                Instruction::Inc { r, next } => {
+                    // Increments the r register
+                    self.registers[*r] += 1;
+                    self.pc = *next; // Go to next address
+                }
+                Instruction::Halt => break,
+            }
         }
+
+        Ok(())
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Instruction {
     /// # Decrement instruction
-    ///
-    /// もしも1つ目のレジスタが0よりも大きかったら2番目に指定した行へ行く。0以下だったら三番目に指定した行へ行く。
     Dec {
         /// Register number
-        r: usize,
+        r: RegisterNumber,
 
-        /// Line number
-        if_big_line: usize,
+        /// Line address if the decrement is success
+        next: LineNumber,
 
-        /// Line number
-        if_small_line: usize,
+        /// Line address if it is zero
+        branch: LineNumber,
     },
 
     /// # Increment instruction
     ///
     /// 指定したレジスタに1を足し、指定した行へと移動。
-    ///
-    /// 1. レジスタ番号
-    /// 1. 行
-    Inc(usize, usize),
+    Inc {
+        /// Register number
+        r: RegisterNumber,
+
+        /// Next line number
+        next: LineNumber,
+    },
 
     /// # Halt instruction
     ///
     /// マシンを停止する。
     Halt,
 }
+
+pub trait Runnable {
+    fn r(&self) -> Result<RegisterNumber, Box<dyn std::error::Error>>;
+    fn next(&self) -> Result<LineNumber, Box<dyn std::error::Error>>;
+    fn branch(&self) -> Result<LineNumber, Box<dyn std::error::Error>>;
+}
+
+impl Runnable for Instruction {
+    fn r(&self) -> Result<RegisterNumber, Box<dyn std::error::Error>> {
+        match self {
+            Self::Dec { r, .. } => Ok(*r),
+            Self::Inc { r, .. } => Ok(*r),
+            _ => Err(Box::new(RunnableError::RegisterNumberNotFound)),
+        }
+    }
+
+    fn next(&self) -> Result<LineNumber, Box<dyn std::error::Error>> {
+        match self {
+            Self::Dec { next, .. } => Ok(*next),
+            Self::Inc { next, .. } => Ok(*next),
+            _ => Err(Box::new(RunnableError::NextNotFound)),
+        }
+    }
+
+    fn branch(&self) -> Result<LineNumber, Box<dyn std::error::Error>> {
+        match self {
+            Self::Dec { branch, .. } => Ok(*branch),
+            _ => Err(Box::new(RunnableError::BranchNotFound)),
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+enum RunnableError {
+    #[error("the runnable value does not have a register number")]
+    RegisterNumberNotFound,
+
+    #[error("the runnable value does not have a next line address")]
+    NextNotFound,
+
+    #[error("the runnable value does not have a branch line address")]
+    BranchNotFound,
+}
+
+type Register = usize;
+type ProgramCounter = usize;
+type RegisterNumber = usize;
+type LineNumber = usize;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use crate::machine::{CounterMachine, Instruction, RegVM};
+
 pub mod machine;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut machine = RegVM::new(
+        vec![22, 11],
+        vec![
+            Instruction::Dec {
+                r: 1,
+                next: 1,
+                branch: 2,
+            },
+            Instruction::Inc { r: 0, next: 0 },
+            Instruction::Halt,
+        ],
+    );
+
+    machine.run()?;
+    dbg!(machine);
+
+    Ok(())
 }
